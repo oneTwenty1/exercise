bf552375daa78dae764a0288d105b0eb54ebd596
oneTwenty1 <171952814@163.com>
2017-03-23 09:30:23 +0800

    determine if it is a palindrome

---
+++ ListNode.java
@@ -0,0 +1,86 @@
+import java.lang.System.*;
+
+
+/**
+ *test for a palindrome
+ */
+public class ListNode{
+
+
+    int val;
+    ListNode next;
+
+
+    public ListNode(int val){
+        this.val = val;
+        this.next = null;
+    }
+
+    /**
+     *
+     *判断是否为回声链表
+     */
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (slow != null && fast != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode tail = reverseList(slow);
+
+        while (head != slow) {
+            if (head.val != slow.val) {
+                return false;
+            }
+            head = head.next;
+            slow = slow.next;
+        }
+
+        return true;
+    }
+
+    /**
+     * 链表的翻转
+     */
+    public static ListNode reverseList(ListNode head) {    
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode first = new ListNode(0);
+        while (head.next != null) {
+            ListNode tmp = head.next.next;
+            first = head.next;
+            first.next = head;
+            head.next = tmp;
+        }
+        
+        return first;
+
+    }
+
+    public static void main(String[] args) {
+        int xlength = args.length();
+        //for (int i = 0; i < xlength; i++) {
+        //    ListNode[] node
+        //}
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        //ListNode c = new ListNode(3);
+        a.next = b;
+        //b.next = c;
+        ListNode z = reverseList(a);
+        System.out.println(z.val);
+    }
+}
+
